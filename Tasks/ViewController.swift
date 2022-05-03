ed958bebc3f3d4fda77326f338d614a57a7e49e9
Ruslan <ruslan.shtybaev@gmail.com>
2022-05-03 15:41:24 +0300

    Initial Commit

---
+++ Tasks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tasks
+//
+//  Created by Руслан Штыбаев on 03.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
