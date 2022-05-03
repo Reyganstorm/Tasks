265b294faf7fa057e8f0b4c7cf9247cdbb426bba
Ruslan <ruslan.shtybaev@gmail.com>
2022-05-03 17:30:56 +0300

    add and setup new ModelFile for TaskList

---
+++ Tasks/Models/TaskList.swift
@@ -0,0 +1,23 @@
+//
+//  TaskList.swift
+//  Tasks
+//
+//  Created by Руслан Штыбаев on 03.05.2022.
+//
+
+import RealmSwift
+
+// Object - это тип данных фреймворка Realm
+class TaskList: Object {
+    @Persisted var name = ""
+    @Persisted var date = Date()
+    let tasks = List<Task>()
+}
+
+class Task: Object {
+    @Persisted var name = ""
+    @Persisted var note = ""
+    @Persisted var date = Date()
+    @Persisted var isCompleted = false
+    
+}
